51309119600bcea1340b74463f93b54f09e1d4f1
Алексей Копасов <alesey.k1@gmail.com>
2025-03-17 22:50:58 +0300

    рефакторит ui формы

--- backend/src/router/createIdea/index.ts
+++ backend/src/router/createIdea/index.ts
@@ -3,6 +3,10 @@ import { trpc } from '../../lib/trpc'
 import { zCreateIdeaTrpcInput } from './input'
 
 export const createIdeaTrpcRoute = trpc.procedure.input(zCreateIdeaTrpcInput).mutation(({ input }) => {
+  if (ideas.find((idea) => idea.nick === input.nick)) {
+    throw new Error('Idea with the same nick already exists')
+  }
+
   ideas.unshift(input)
   return true
 })
--- webapp/src/components/Textarea/index.tsx
+++ webapp/src/components/Textarea/index.tsx
@@ -1,24 +1,41 @@
+import cn from 'classnames'
 import { type FormikProps } from 'formik'
+import css from './index.module.scss'
 
 export const Textarea = ({ name, label, formik }: { name: string; label: string; formik: FormikProps<any> }) => {
   const error = formik.errors[name] as string | undefined
   const touched = formik.touched[name]
+  const disabled = formik.isSubmitting
+  const invalid = !!error && !!touched
 
   return (
-    <div style={{ marginBottom: 10 }}>
-      <label htmlFor="{name}">{label}</label>
-      <br />
+    <div
+      className={cn({
+        [css.field]: true,
+        [css.disabled]: disabled,
+      })}
+    >
+      <label className={css.label} htmlFor="{name}">
+        {label}
+      </label>
+
       <textarea
+        className={cn({
+          [css.input]: true,
+          [css.invalid]: invalid,
+        })}
         onChange={(e) => {
           void formik.setFieldValue(name, e.target.value)
         }}
         onBlur={() => {
           void formik.setFieldTouched(name)
         }}
+        value={formik.values[name]}
         name={name}
         id={name}
+        disabled={disabled}
       />
-      {!!error && !!touched && <div style={{ color: 'red' }}>{error}</div>}
+      {invalid && <div className={css.error}>{error}</div>}
     </div>
   )
 }
