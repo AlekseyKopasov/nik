023f5265a78a43a629a72f7650b607c40f85a2b6
Алексей Копасов <alesey.k1@gmail.com>
2025-03-18 23:48:04 +0300

    создает конекст бэкенд приложения, в котором будет хранитсья подключение к Prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,30 @@
 import cors from 'cors'
 import express from 'express'
+import { type AppContext, createAppContext } from './lib/ctx'
 import { applyTrpcToExpressApp } from './lib/trpc'
 import { trpcRouter } from './router'
 
-const expressApp = express()
-expressApp.use(cors())
+let ctx: AppContext | null = null
 
-expressApp.get('/ping', (req, res) => {
-  res.send('pong')
-})
+void (async () => {
+  try {
+    ctx = createAppContext()
 
-applyTrpcToExpressApp(expressApp, trpcRouter)
+    const expressApp = express()
+    expressApp.use(cors())
 
-expressApp.listen(3000, () => {
-  console.info('Listening at http://localhost:3000')
-})
+    expressApp.get('/ping', (req, res) => {
+      res.send('pong')
+    })
+
+    applyTrpcToExpressApp(expressApp, ctx, trpcRouter)
+
+    expressApp.listen(3000, () => {
+      console.info('Listening at http://localhost:3000')
+    })
+  } catch (error) {
+    console.error(error)
+
+    await ctx?.stop()
+  }
+})()
--- backend/src/lib/trpc.ts
+++ backend/src/lib/trpc.ts
@@ -2,14 +2,16 @@ import { initTRPC } from '@trpc/server'
 import * as trpsExpress from '@trpc/server/adapters/express'
 import { type Express } from 'express'
 import { type TrpcRouter } from '../router'
+import { type AppContext } from './ctx'
 
-export const trpc = initTRPC.create()
+export const trpc = initTRPC.context<AppContext>().create()
 
-export const applyTrpcToExpressApp = (expressApp: Express, trpcRouter: TrpcRouter) => {
+export const applyTrpcToExpressApp = (expressApp: Express, appContext: AppContext, trpcRouter: TrpcRouter) => {
   expressApp.use(
     '/trpc',
     trpsExpress.createExpressMiddleware({
       router: trpcRouter,
+      createContext: () => appContext,
     })
   )
 }
