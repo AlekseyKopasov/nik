9844f1c208502b669844a6ae093c6fea2e31fbed
Алексей Копасов <alesey.k1@gmail.com>
2025-03-25 23:43:52 +0300

    обновляет sass

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,7 +1,19 @@
 import react from '@vitejs/plugin-react'
+import sass from 'sass'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+        sassOptions: {
+          fiber: false,
+          quietDeps: true,
+        },
+      },
+    },
+  },
 })
